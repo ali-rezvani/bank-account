b93c28dc1e7f6b3ae396a6a4ed53f29229baf4ae
computer <a.rezvani2006@gmail.com>
2024-08-03 23:07:32 +0330

    event store

---
+++ account-command/src/main/java/com/techbank/account/command/domain/EventStoreRepository.java
@@ -0,0 +1,13 @@
+package com.techbank.account.command.domain;
+
+import com.techbank.cqrs.core.events.EventModel;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface EventStoreRepository extends MongoRepository<EventModel, UUID> {
+    List<EventModel> findByAggregateIdentifier(String aggregateIdentifier);
+}
---
+++ account-command/src/main/java/com/techbank/account/command/infrastructure/AccountEventStore.java
@@ -0,0 +1,59 @@
+package com.techbank.account.command.infrastructure;
+
+import com.techbank.account.command.domain.AccountAggregate;
+import com.techbank.account.command.domain.EventStoreRepository;
+import com.techbank.cqrs.core.events.BaseEvent;
+import com.techbank.cqrs.core.events.EventModel;
+import com.techbank.cqrs.core.exception.AggregateNotFoundException;
+import com.techbank.cqrs.core.exception.ConcurrencyException;
+import com.techbank.cqrs.core.infrastructure.EventStore;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.ConcurrencyFailureException;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class AccountEventStore implements EventStore {
+    private final EventStoreRepository eventStoreRepository;
+    @Override
+    public void saveEvents(String aggregateId, Iterable<BaseEvent> events, int expectedVersion) {
+        var eventStream=eventStoreRepository.findByAggregateIdentifier(aggregateId);
+        if (expectedVersion !=-1 && eventStream.get(eventStream.size()-1).getVersion()!=expectedVersion){
+            throw new ConcurrencyException();
+        }
+        var version=expectedVersion;
+        for (var event : events) {
+            version++;
+            event.setVersion(version);
+            var eventModel= EventModel.builder()
+                    .id(event.getId())
+                    .aggregateIdentifier(aggregateId)
+                    .aggregateType(AccountAggregate.class.getTypeName())
+                    .version(version)
+                    .eventType(event.getClass().getTypeName())
+                    .timestamp(Instant.now())
+                    .eventData(event)
+                    .build();
+            var persistedEvent=eventStoreRepository.save(eventModel);
+            if (persistedEvent!=null){
+//                TODO produce event to kafka
+            }
+        }
+    }
+
+    @Override
+    public List<BaseEvent> getEvents(String aggregateId) {
+        var eventStream=eventStoreRepository.findByAggregateIdentifier(aggregateId);
+        if (eventStream.isEmpty()){
+            throw new AggregateNotFoundException("Incorrect account ID provided.");
+        }
+        return eventStream
+                .stream()
+                .map(EventModel::getEventData)
+                .collect(Collectors.toList());
+    }
+}
